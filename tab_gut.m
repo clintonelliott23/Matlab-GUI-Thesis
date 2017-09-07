fd248e62f70fac38e16880e2f5c75fef4905f228
clintonelliott23 <clintonelliott23@hotmail.com>
2017-09-07 20:42:14 +1000

    Needs to be Commented Now
    Deffinately Future me problem

--- tab_gut.m
+++ tab_gut.m
@@ -23,30 +23,33 @@
 % end
 % 
 % disp(IRR_val*100)
-     
-state_codes = [4814 4825 0800 6000 3000 7000 2000 4000];
-state_names = {'Townsville, QLD'; 'Mount Isa, QLD'; 'Darwin, NT'; 'Perth, WA'; 'Melbourne, VIC';...
-    'Horbart, TAS'; 'Sydney, NSW'; 'Brisbane, QLD'};
+%      
+% state_codes = [4814 4825 0800 6000 3000 7000 2000 4000];
+% state_names = {'Townsville, QLD'; 'Mount Isa, QLD'; 'Darwin, NT'; 'Perth, WA'; 'Melbourne, VIC';...
+%     'Horbart, TAS'; 'Sydney, NSW'; 'Brisbane, QLD'};
+% 
+% f = figure('Position',[100 100 400 150]);
+% state_names = {'Townsville, QLD'; 'Mount Isa, QLD'; 'Darwin, NT'; 'Perth, WA'; 'Melbourne, VIC';...
+%     'Horbart, TAS'; 'Sydney, NSW'; 'Brisbane, QLD'};
+% checkbox1= num2cell([4814; 4825; 0800])
+% yourdata =[num2cell(state_codes') state_names]  
+% columnname =   {'Postcode', 'Location'};
+% columnformat = {'char', 'char'};
+% columneditable =  [true false]; 
+% t = uitable('Units','normalized','Position',...
+%           [0.1 0.1 0.9 0.9], 'Data', yourdata,... 
+%           'ColumnName', columnname,...
+%           'ColumnFormat', columnformat,...
+%           'ColumnEditable', columneditable,...
+%           'RowName',[] ,'BackgroundColor',[.7 .9 .8],...
+%           'ForegroundColor',[0 0 0],'ColumnWidth',{100 155});
 
-f = figure('Position',[100 100 400 150]);
-state_names = {'Townsville, QLD'; 'Mount Isa, QLD'; 'Darwin, NT'; 'Perth, WA'; 'Melbourne, VIC';...
-    'Horbart, TAS'; 'Sydney, NSW'; 'Brisbane, QLD'};
-checkbox1= num2cell([4814; 4825; 0800])
-yourdata =[num2cell(state_codes') state_names]  
-columnname =   {'Postcode', 'Location'};
-columnformat = {'char', 'char'};
-columneditable =  [true false]; 
-t = uitable('Units','normalized','Position',...
-          [0.1 0.1 0.9 0.9], 'Data', yourdata,... 
-          'ColumnName', columnname,...
-          'ColumnFormat', columnformat,...
-          'ColumnEditable', columneditable,...
-          'RowName',[] ,'BackgroundColor',[.7 .9 .8],...
-          'ForegroundColor',[0 0 0],'ColumnWidth',{100 155});
+ for i = 1:1:360
       
+      x(1,i) = 65000*sind(i) + 65000
+ end
       
-      
-      
+    plot(x)   
       
       
       
